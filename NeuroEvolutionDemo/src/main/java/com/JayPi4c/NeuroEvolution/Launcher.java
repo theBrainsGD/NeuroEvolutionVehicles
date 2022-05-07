3eafb885fb88a2944fd257a7223e73c64a8deb4c
Jonas Pohl <jonas.jp4c@gmail.com>
2022-05-07 12:22:14 +0200

    prerelease Build

--- NeuroEvolutionDemo/src/main/java/com/JayPi4c/NeuroEvolution/Launcher.java
+++ NeuroEvolutionDemo/src/main/java/com/JayPi4c/NeuroEvolution/Launcher.java
@@ -1,5 +1,9 @@
 package com.JayPi4c.NeuroEvolution;
 
+/**
+ * Lauches the whole program.
+ * @author JayPi4c
+ */
 public class Launcher {
 	public static void main(String[] args) {
 		App.main(args);
