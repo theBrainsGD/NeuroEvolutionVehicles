32ef5e9f2c1baaeccced62c725cffb14cd60bcb0
Jonas Pohl <jonas.jp4c@gmail.com>
2022-05-07 12:52:52 +0200

    Adding tagged release job

--- NeuroEvolutionDemo/src/main/java/com/JayPi4c/NeuroEvolution/Launcher.java
+++ NeuroEvolutionDemo/src/main/java/com/JayPi4c/NeuroEvolution/Launcher.java
@@ -2,12 +2,14 @@ package com.JayPi4c.NeuroEvolution;
 
 /**
  * Lauches the whole program.
+ * 
  * @author JayPi4c
  */
 public class Launcher {
-	
+
 	/**
 	 * Launches the applications main method with the given arguments.
+	 * 
 	 * @param args The arguments to pass to the main method.
 	 */
 	public static void main(String[] args) {
