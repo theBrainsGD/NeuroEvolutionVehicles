73237710dc7850e35dd7603ad3f5e6f176b94e98
Jonas Pohl <jonas.jp4c@gmail.com>
2022-05-07 12:26:59 +0200

    Fixing build files

--- NeuroEvolutionDemo/src/main/java/com/JayPi4c/NeuroEvolution/Launcher.java
+++ NeuroEvolutionDemo/src/main/java/com/JayPi4c/NeuroEvolution/Launcher.java
@@ -5,6 +5,11 @@ package com.JayPi4c.NeuroEvolution;
  * @author JayPi4c
  */
 public class Launcher {
+	
+	/**
+	 * Launches the applications main method with the given arguments.
+	 * @param args The arguments to pass to the main method.
+	 */
 	public static void main(String[] args) {
 		App.main(args);
 	}
